4be722f988ce82785948179575485c5b4a15633a
Alfred Calayag & Alfred Calayag <pair+alfredcalayag.alfredcalayag@devbootcamp.com>
2014-12-18 16:20:04 -0800

    Add alternating chip colors

---
+++ View/view.js
@@ -0,0 +1,24 @@
+  $(document).ready(function() {
+
+    var currentColor = "black"
+
+    var nextColor = function(currentColor){
+      if (currentColor == "red")
+        return "black"
+      else if (currentColor == "black")
+        return "red"
+      else
+        return "red"
+    }
+
+    //if black, then nextPlayer = red
+    //if red, then nextPlayer = black
+
+    $("button").click(function() {
+      var columnId = $(this).parent().index();
+      console.log(columnId);
+      $(this).parent().children(".empty").last().addClass(currentColor).removeClass("empty");
+      currentColor = nextColor(currentColor)
+      console.log(currentColor)
+    })
+  })
